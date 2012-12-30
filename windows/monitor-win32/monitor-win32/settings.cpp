2f9a9e0d912264126182e216f0868fbd597e047f
Christopher Hoobin <christopher.hoobin@rmit.edu.au>
2012-12-30 21:33:26 +1100

    Added ReadFile/WriteFile function call error checking.

--- windows/monitor-win32/monitor-win32/settings.cpp
+++ windows/monitor-win32/monitor-win32/settings.cpp
@@ -1,20 +1,51 @@
 #include <windows.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include <Shlwapi.h> // PathAppend, PathFileExists
 #pragma comment(lib, "Shlwapi.lib")
 
 #include <shlobj.h> // SHGetKnownFolderPath
 
+#include <vector>
+#include <string>
+using std::vector;
+using std::wstring;
+
 #include "application.h"
 #include "menu.h"
 #include "resource.h"
 #include "settings.h"
 
+#define ___IO(operation, handle, value, nbytes) do {                \
+    DWORD read;                                                     \
+    operation(handle, value, nbytes, &read, NULL);                  \
+    assert(read == nbytes);                                         \
+__pragma(warning(push))                                             \
+__pragma(warning(disable:4127))                                     \
+    } while (0)  /* C4127: conditional expression is constant. */   \
+__pragma(warning(pop))
+
+#define Read(handle, value) ___IO(ReadFile, handle, &value, sizeof value)
+
+#define Write(handle, value) ___IO(WriteFile, handle, &value, sizeof value)
+
+#define ReadPtr(handle, value, nbytes) ___IO(ReadFile, handle, value, nbytes)
+
+#define WritePtr(handle, value, nbytes) ___IO(WriteFile, handle, value, nbytes)
+
 static void
 default_settings()
 {
-    application.font.type = CreateFont(
+    application.settings.interface_id = ID_MENU_INTERFACE;
+
+    application.settings.units_id = ID_MENU_UNITS_KPS;
+
+    application.settings.prefix_id = ID_MENU_PREFIX_SI;
+
+    application.settings.placement_id = ID_MENU_PLACEMENT_TR;
+
+    application.settings.font.family = CreateFont(
         14,
         0,
         0,
@@ -30,12 +61,9 @@ default_settings()
         DEFAULT_PITCH,
         L"Courier New");
 
-    // XXX What if the desktop background is white?
-    application.font.color = RGB(255, 255, 255);
-
-    application.placement_id = ID_MENU_PLACEMENT_TR;
-
-    application.interface_id = ID_MENU_INTERFACE;
+    // TODO What if the desktop background is white?
+    // Computer the average color of the desktop background and adjust default color to white or black?
+    application.settings.font.color = RGB(255, 255, 255);
 }
 
 static void
@@ -61,7 +89,7 @@ save_path(wchar_t *path)
 void
 load_settings()
 {
-    application.interface_names = application.monitor.interfaces();
+    vector<wstring> interface_names = application.monitor.interfaces();
 
     wchar_t path[BUFSIZ];
 
@@ -81,35 +109,46 @@ load_settings()
         return;
     }
 
-    DWORD nbytes;
+    uint16_t magic;
 
-    LOGFONT logfont;
+    Read(file, magic);
 
-    ReadFile(file, &logfont, sizeof logfont, &nbytes, NULL);
+    if (magic != MONITOR_MAGIC) {
+        default_settings();
+        return;
+    }
 
-    application.font.type = CreateFontIndirect(&logfont);
-
-    ReadFile(file, &application.font.color, sizeof application.font.color, &nbytes, NULL);
-
-    ReadFile(file, &application.placement_id, sizeof application.placement_id, &nbytes, NULL);
-
-    ReadFile(file, &application.interface_id, sizeof application.interface_id, &nbytes, NULL);
+    Read(file, application.settings.interface_id);
 
     size_t length = 0;
 
-    ReadFile(file, &length, sizeof length, &nbytes, NULL);
+    Read(file, length);
 
     wchar_t buffer[BUFSIZ] = {0};
 
-    ReadFile(file, buffer, length, &nbytes, NULL);
+    ReadPtr(file, buffer, length);
 
-    if (wcsncmp(buffer, application.interface_names[INTERFACE_INDEX(application.interface_id)].c_str(), length) != 0) {
+    if (wcsncmp(buffer, interface_names[INTERFACE_INDEX(application.settings.interface_id)].c_str(), length) != 0) {
         // If the interface string does not match the value of the saved index there has been a hardware change.
         // So we reset the value to the default 'All Interfaces' value (learnt this on the hard way :D).
-        application.interface_id = ID_MENU_INTERFACE;
+        application.settings.interface_id = ID_MENU_INTERFACE;
     }
 
-    application.monitor.switch_interface(INTERFACE_INDEX(application.interface_id));
+    application.monitor.switch_interface(INTERFACE_INDEX(application.settings.interface_id));
+
+    Read(file, application.settings.units_id);
+
+    Read(file, application.settings.prefix_id);
+
+    Read(file, application.settings.placement_id);
+
+    LOGFONT logfont;
+
+    Read(file, logfont);
+
+    application.settings.font.family = CreateFontIndirect(&logfont);
+
+    Read(file, application.settings.font.color);
 
     CloseHandle(file);
 }
@@ -117,6 +156,8 @@ load_settings()
 void
 save_settings()
 {
+    vector<wstring> interface_names = application.monitor.interfaces();
+
     wchar_t path[BUFSIZ];
 
     save_path(path);
@@ -130,27 +171,35 @@ save_settings()
         FILE_ATTRIBUTE_NORMAL,
         NULL);
 
-    DWORD nbytes;
+    assert(file != INVALID_HANDLE_VALUE);
 
-    LOGFONT logfont;
+    uint16_t magic = MONITOR_MAGIC;
 
-    GetObject(application.font.type, sizeof logfont, &logfont);
+    Write(file, magic);
 
-    WriteFile(file, &logfont, sizeof logfont, &nbytes, NULL);
+    Write(file, application.settings.interface_id);
 
-    WriteFile(file, &application.font.color, sizeof application.font.color, &nbytes, NULL);
-
-    WriteFile(file, &application.placement_id, sizeof application.placement_id, &nbytes, NULL);
-
-    WriteFile(file, &application.interface_id, sizeof application.interface_id, &nbytes, NULL);
-
-    size_t length = application.interface_names[INTERFACE_INDEX(application.interface_id)].length();
+    size_t length = interface_names[INTERFACE_INDEX(application.settings.interface_id)].length();
 
     length *= sizeof(wchar_t);
 
-    WriteFile(file, &length, sizeof length, &nbytes, NULL);
+    Write(file, length);
 
-    WriteFile(file, application.interface_names[INTERFACE_INDEX(application.interface_id)].c_str(), length, &nbytes, NULL);
+    WritePtr(file, interface_names[INTERFACE_INDEX(application.settings.interface_id)].c_str(), length);
+
+    Write(file, application.settings.units_id);
+
+    Write(file, application.settings.prefix_id);
+
+    Write(file, application.settings.placement_id);
+
+    LOGFONT logfont;
+
+    GetObject(application.settings.font.family, sizeof logfont, &logfont);
+
+    Write(file, logfont);
+
+    Write(file, application.settings.font.color);
 
     CloseHandle(file);
 }
